a464442faffc127909034d2a42afc34eb7e963ab
Rinat7002 <rinat_idrisov_2016@mail.ru>
2024-03-10 21:45:24 +0500

    реализован header, секция promo

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".promo__swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".promo .arrow_next",
+        prevEl: ".promo .arrow_prew",
+      },
+});
